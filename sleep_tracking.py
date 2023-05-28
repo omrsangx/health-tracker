a059088cc9368358b4fc06c962003f97dc3268e1
omrsangx <28844245+omrsangx@users.noreply.github.com>
2023-05-28 13:20:56 -0400

    Add files via upload

---
+++ sleep_tracking.py
@@ -0,0 +1,130 @@
+# Sleep Tracking App
+# Author: omrsangx
+import string
+import re
+import json
+import random
+#import datetime
+from datetime import datetime
+# import time
+from flask import Flask, render_template, Response, request, json, escape
+from sleep_tracking_db_class import sleep_db_class # Importing the class for the database object
+import sqlite3
+
+app = Flask(__name__)
+
+database_name_sleep = './databases/sleep_database_pro_app.db'
+database_table_name = 'sleep_table'
+wakeup_table_name = 'wakeup_table'
+sleep_duration_table_name = 'duration_table'
+
+
+# Creating database object
+db_obj_sleep = sleep_db_class(database_name_sleep,database_table_name)
+db_obj_wakeup = sleep_db_class(database_name_sleep,wakeup_table_name)
+db_obj_duration = sleep_db_class(database_name_sleep,sleep_duration_table_name)
+
+# Creating database sleep and wake tables
+db_obj_sleep.create_sleep_table()
+db_obj_wakeup.create_wakeup_table()
+db_obj_duration.create_sleep_duration_table()
+
+def change_to_datetime_obj(value):
+    datetime_object = datetime.fromisoformat(str(value))
+    return str(datetime_object)
+
+# Index route, function
+@app.route("/")
+def index():
+    html_file = "index.html" 
+
+    def get_month_day_function(value):
+        change_back_to_date = datetime.fromisoformat(str(value))
+        return str(change_back_to_date.month) + "/" + str(change_back_to_date.day)
+    
+    limit_value = 7
+    sleep_duration_seven_days = db_obj_duration.lookup_by_limit_sleep_duration(limit_value)
+    get_month_day_list = list()
+    duration_hours_list = list()
+    get_sleep_quality = db_obj_wakeup.lookup_quality()
+    sleep_quality_string = re.sub('[(,\')]', '', str(get_sleep_quality[0]))
+    print(type(sleep_quality_string))
+    for row in reversed(sleep_duration_seven_days):
+        get_month_day_list.append(get_month_day_function(row[0]))
+        duration_hours_list.append(row[1])
+
+    sleep_durations_data = json.dumps(duration_hours_list)
+    sleep_duration_dates_labels = json.dumps(get_month_day_list)
+    print("sleep duration:", sleep_durations_data)
+    print("days of the week:", sleep_duration_dates_labels)
+    return render_template(html_file, data=sleep_durations_data, labels=sleep_duration_dates_labels, sleep_quality_str=sleep_quality_string)  
+
+@app.route("/wba-sleep")
+def wba_sleep():
+    html_file = "sleep.html" 
+    return render_template(html_file)
+
+@app.route("/wba-wakeup")
+def wba_wakeup():
+    html_file = "wakeup.html" 
+    return render_template(html_file)
+
+# Sleep route, function
+@app.route("/sleepform", methods=["POST"])
+def sleep_func():
+    sleep_date_time = datetime.now()
+    sleep_sex =  str(escape(request.form.get('sleep_sex')))
+    sleep_exercise = str(escape(request.form.get('sleep_exercise')))
+    sleep_bath_one = str(escape(request.form.get('sleep_bath_one')))
+    sleep_bath_two = str(escape(request.form.get('sleep_bath_two')))
+    sleep_home = str(escape(request.form.get('sleep_home')))
+    sleep_slept_day = str(escape(request.form.get('sleep_slept_day')))
+    sleep_screen = str(escape(request.form.get('sleep_screen')))
+    sleep_meds = str(escape(request.form.get('sleep_meds')))
+    sleep_drink = str(escape(request.form.get('sleep_drink')))
+    sleep_drive = str(escape(request.form.get('sleep_drive')))
+    sleep_work = str(escape(request.form.get('sleep_work')))
+    sleep_alarm = str(escape(request.form.get('sleep_alarm')))
+    sleep_eat = str(escape(request.form.get('sleep_eat')))
+    unknown_x = str(escape(request.form.get('unknown_x')))
+    db_obj_sleep.insert_record_sleep_table(sleep_date_time, sleep_sex, sleep_exercise, sleep_bath_one, sleep_bath_two, sleep_home, sleep_slept_day, sleep_screen, sleep_meds, sleep_drink, sleep_drive, sleep_work, sleep_alarm, sleep_eat, unknown_x)
+    
+    html_form_submitted = "landing_page.html"
+    return render_template(html_form_submitted)
+
+# Wakup route, function
+@app.route("/wakeupform", methods=["POST"])
+def wakeup_func():
+    wakeup_date_time = datetime.now()
+
+    zero_place_holder = float(0)
+    limit_value = 1
+    sleep_date = db_obj_sleep.lookup_by_limit(limit_value)
+    get_date_db = list()
+    for row in sleep_date:
+        get_date_db.append(row[0])
+    
+    sleep_datetime_data = get_date_db[0]
+    print(sleep_datetime_data)
+    last_sleep_datetime = datetime.fromisoformat(str(sleep_datetime_data))
+    now_datetime = datetime.now()
+    sleep_duration = now_datetime - last_sleep_datetime
+    total_hours_asleep = format(sleep_duration.total_seconds() / 3600, ".2f")
+
+
+    if float(total_hours_asleep) < float(17):
+        db_obj_duration.insert_record_sleep_duration_table(now_datetime, total_hours_asleep)
+    else:
+        print("log: zero place holder added to db")
+        db_obj_duration.insert_record_sleep_duration_table(now_datetime, zero_place_holder)
+    
+    wakeup_dream = str(escape(request.form.get('wakeup_dream')))
+    wakeup_alarm = str(escape(request.form.get('wakeup_alarm')))
+    sleep_quality = str(escape(request.form.get('sleep_quality')))
+    db_obj_wakeup.insert_record_wakeup_table(wakeup_date_time, wakeup_dream, wakeup_alarm, sleep_quality)
+    html_form_submitted = "landing_page.html"
+    return render_template(html_form_submitted) 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9090, debug=True)
+
---
+++ sleep_tracking_db_class.py
@@ -0,0 +1,108 @@
+# Sleep Tracker Database Class
+
+import sqlite3
+
+class sleep_db_class:
+
+    def __init__(self, db_name, table_name):
+        self.database_name = db_name
+        self.table_name = table_name
+
+    def open_db_connection(self):
+        self.db_connection = sqlite3.connect(self.database_name)
+        self.db_cursor = self.db_connection.cursor()
+    
+    def close_db_connection(self):
+        self.db_connection.commit()
+        self.db_connection.close()        
+    
+    # Sleep - Create Table 
+    def create_sleep_table(self): # database schima
+        self.open_db_connection()
+        self.db_cursor.execute(f''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{str(self.table_name)}' ''')
+        #if the count is 1, then table exists
+        if self.db_cursor.fetchone()[0]==1 : {
+            print(f'{str(self.table_name)} table exists')
+        }
+        else:
+            # self.db_cursor.execute(f'DROP TABLE IF EXISTS {str(self.table_name)}')
+            self.db_cursor.execute(f'CREATE TABLE {str(self.table_name)} (datetime NUMBERIC, sleep_sex TEXT, sleep_exercise TEXT, sleep_bath_one TEXT, sleep_bath_two TEXT, sleep_home TEXT, sleep_slept_day TEXT, sleep_screen TEXT, sleep_meds TEXT, sleep_drink TEXT, sleep_drive TEXT, sleep_work TEXT, sleep_alarm TEXT, sleep_eat TEXT, unknown_x TEXT)')
+        self.close_db_connection()
+
+    # Wakeup - Create Table
+    def create_wakeup_table(self):
+        self.open_db_connection()
+        self.db_cursor.execute(f''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{str(self.table_name)}' ''')
+        #if the count is 1, then table exists
+        if self.db_cursor.fetchone()[0]==1 : {
+            print(f'{str(self.table_name)} table exists')
+        }
+        else:
+            # self.db_cursor.execute(f'DROP TABLE IF EXISTS {str(self.table_name)}')
+            self.db_cursor.execute(f'CREATE TABLE {str(self.table_name)} (datetime NUMBERIC, wakeup_dream TEXT, wakeup_alarm TEXT, sleep_quality TEXT)')
+        self.close_db_connection()
+
+    # Sleep Duration - Create Table
+    def create_sleep_duration_table(self):
+        self.open_db_connection()
+        self.db_cursor.execute(f''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{str(self.table_name)}' ''')
+        #if the count is 1, then table exists
+        if self.db_cursor.fetchone()[0]==1 : {
+            print(f'{str(self.table_name)} table exists')
+        }
+        else:
+            self.db_cursor.execute(f'CREATE TABLE {str(self.table_name)} (datetime NUMBERIC, duration REAL)')
+        self.close_db_connection()
+
+    # Sleep - Insert Record    
+    def insert_record_sleep_table(self, datetime, sleep_sex, sleep_exercise, sleep_bath_one, sleep_bath_two, sleep_home, sleep_slept_day, sleep_screen, sleep_meds, sleep_drink, sleep_drive, sleep_work, sleep_alarm, sleep_eat, unknown_x):
+        self.open_db_connection()
+        self.db_cursor.execute(f"INSERT INTO {str(self.table_name)} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (datetime, sleep_sex, sleep_exercise, sleep_bath_one, sleep_bath_two, sleep_home, sleep_slept_day, sleep_screen, sleep_meds, sleep_drink, sleep_drive, sleep_work, sleep_alarm, sleep_eat, unknown_x))
+        self.close_db_connection()
+    
+    # Wakeup - Insert Record
+    def insert_record_wakeup_table(self, datetime, wakeup_dream, wakeup_alarm, sleep_quality):
+        self.open_db_connection()
+        self.db_cursor.execute(f"INSERT INTO {str(self.table_name)} VALUES (?,?,?,?)", (datetime, wakeup_dream, wakeup_alarm, sleep_quality))
+        self.close_db_connection()
+
+    # Sleep Duration - Insert Record
+    def insert_record_sleep_duration_table(self, datetime, duration):
+        self.open_db_connection()
+        self.db_cursor.execute(f"INSERT INTO {str(self.table_name)} VALUES (?,?)", (datetime, duration))
+        self.close_db_connection()
+        
+    def show_all_records(self):
+        self.open_db_connection()
+        self.db_cursor.execute(f"SELECT rowid, * FROM {str(self.table_name)} ORDER BY rowid") 
+        self.items = self.db_cursor.fetchall()
+        self.close_db_connection()
+        return self.items
+
+    def lookup_last_seven_days(self):
+        self.open_db_connection()
+        self.db_cursor.execute(f"SELECT datetime FROM {str(self.table_name)} ORDER BY datetime DESC LIMIT 7")
+        self.items = self.db_cursor.fetchall()
+        self.close_db_connection()
+        return self.items
+    
+    def lookup_by_limit(self, limit):
+        self.open_db_connection()
+        self.db_cursor.execute(f"SELECT datetime FROM {str(self.table_name)} ORDER BY rowid DESC LIMIT (?)", (limit,))
+        self.items = self.db_cursor.fetchall()
+        self.close_db_connection()
+        return self.items
+
+    def lookup_by_limit_sleep_duration(self, limit):
+        self.open_db_connection()
+        self.db_cursor.execute(f"SELECT datetime, duration FROM {str(self.table_name)} ORDER BY rowid DESC LIMIT (?)", (limit,))
+        self.items = self.db_cursor.fetchall()
+        self.close_db_connection()
+        return self.items        
+
+    def lookup_quality(self):
+        self.open_db_connection()
+        self.db_cursor.execute(f"SELECT sleep_quality FROM {str(self.table_name)} ORDER BY rowid DESC LIMIT 1")
+        self.items = self.db_cursor.fetchall()
+        self.close_db_connection()
+        return self.items        
